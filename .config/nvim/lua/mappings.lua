371de21d5c9217a3f6cef2163f8a579b1c8fc94d
l0py2 <l0py2.git@gmail.com>
2023-12-24 19:43:31 +0000

    Added mapping for telescope.lsp_document_symbols

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,3 +10,4 @@ local telescope = require('telescope.builtin')
 
 keymap.set('n', '<leader>ff', telescope.find_files, {})
 keymap.set('n', '<leader>fd', telescope.lsp_definitions, {})
+keymap.set('n', '<leader>fs', telescope.lsp_document_symbols, {})
